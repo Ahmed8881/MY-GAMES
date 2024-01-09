c0183c565feaee71a8c18e02e29179e77eb946c0
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-01-09 15:44:15 +0500

    Update game.cpp

--- TIC TAC TOE/game.cpp
+++ TIC TAC TOE/game.cpp
@@ -1,3 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+using namespace std;
+
+char square[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+string setcolor(unsigned short color);
