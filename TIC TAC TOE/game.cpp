4d287c288f794a3f2558ed314c48961188207f36
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-01-09 16:12:46 +0500

    Update game.cpp

--- TIC TAC TOE/game.cpp
+++ TIC TAC TOE/game.cpp
@@ -88,3 +88,28 @@ cout<<" "<<endl;
     cout<<" "<<endl;
     cout<<" "<<endl;
 }
+int checkwin() {
+    if (square[1] == square[2] && square[2] == square[3]) {
+        return 1;
+    } else if (square[4] == square[5] && square[5] == square[6]) {
+        return 1;
+    } else if (square[7] == square[8] && square[8] == square[9]) {
+        return 1;
+    } else if (square[1] == square[4] && square[4] == square[7]) {
+        return 1;
+    } else if (square[2] == square[5] && square[5] == square[8]) {
+        return 1;
+    } else if (square[3] == square[6] && square[6] == square[9]) {
+        return 1;
+    } else if (square[1] == square[5] && square[5] == square[9]) {
+        return 1;
+    } else if (square[3] == square[5] && square[5] == square[7]) {
+        return 1;
+    } else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
+               square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7]
+               != '7' && square[8] != '8' && square[9] != '9') {
+        return 0;
+    } else {
+        return -1;
+    }
+}
