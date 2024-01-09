96469ad40a2a69c0c1ab1deb8dfb373ac882ee42
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-01-09 16:07:12 +0500

    Update game.cpp

--- TIC TAC TOE/game.cpp
+++ TIC TAC TOE/game.cpp
@@ -7,3 +7,6 @@ char square[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 string setcolor(unsigned short color);
 int checkwin();
 void board();
+int main() {
+    int choice;
+    char mark;
